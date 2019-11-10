8667ea85561f2687aab875b815fdfe03afffba3b
aokyut <epaperamic@gmail.com>
2019-11-11 08:49:25 +0900

    update ppo_continuous.py add network that store weights

--- ppo_continuos.py
+++ ppo_continuos.py
@@ -36,10 +36,13 @@ import pybullet_envs
 from time import sleep
 from gym import wrappers
 from os import path
+from copy import deepcopy
+
+import pickle
 
 parser=argparse.ArgumentParser(description="Reiforcement training with PPO",add_help=True)
 parser.add_argument("--model",type=str,required=True,help="model base name. required")
-parser.add_argument("--env_name",default="AntBulletEnv-v0",help="environment name. default is AntBulletEnv-v0")
+parser.add_argument("--env_name",default="HopperBulletEnv-v0",help="environment name. default is AntBulletEnv-v0")
 parser.add_argument("--save",action="store_true",default=False,help="save command")
 parser.add_argument("--load",action="store_true",default=False,help="load command")
 parser.add_argument("--show",action="store_true",default=False,help="render environment")
@@ -53,37 +56,41 @@ WORKER_NUM=args.thread_num
 #define constants
 VIDEO_DIR="./train_info/video"
 MODEL_DIR="./train_info/models"
+SUMMARY_DIR="./logs"
+SUMMARY_PATH=path.join(SUMMARY_DIR, args.model)
 MODEL_SAVE_PATH=path.join(MODEL_DIR,args.model)
 
-ADVANTAGE=4
-STATE_NUM=28
+ADVANTAGE=2
+STATE_NUM=15
 ACTION_LIST=[0,1]
-ACTION_NUM=8
+ACTION_NUM=3
 #epsiron parameter
-EPS_START = 0.8
-EPS_END = 0.4
-EPS_STEPS = 200 * WORKER_NUM**2
+EPS_START = 0.5
+EPS_END = 0.2
+EPS_STEPS = 1000 * WORKER_NUM**2
 #learning parameter
-GAMMA=0.99
-LEARNING_RATE=0.002
+GAMMA=0.98
+LEARNING_RATE=2e-5
 DROPOUT_RATE=0.5
+MAX_BUFFER = 10000
 #loss constants
 LOSS_V=0.5
 LOSS_ENTROPY=0.02
-HIDDEN_LAYERE=64
-LAYERE2=32
-LAYERE3=16
+HIDDEN_LAYERE=256
+LAYERE2=8
+LAYERE3=4
 
 EPSIRON = 0.2
 
 
 #define some of functions
 def huber_loss(advantage, delta=0.5):
-    error = advantage
-    cond = tf.abs(error) < delta
+    with tf.name_scope("huber_loss"):
+        error = advantage
+        cond = tf.abs(error) < delta
 
-    squared_loss = 0.5 * tf.square(error)
-    linear_loss = delta * (tf.abs(error) - 0.5 * delta)
+        squared_loss = 0.5 * tf.square(error)
+        linear_loss = delta * (tf.abs(error) - 0.5 * delta)
     return tf.where(cond, squared_loss, linear_loss)
 
 def random(min,max,size):
@@ -95,77 +102,107 @@ class ppo_brain:
         self.build_model()
         self.name="brain"
         self.prob_old=1.0
+        self.train_num = 0
+        self.memory=[[], [], []]
 
     def build_model(self):
-        self.input = tf.placeholder(dtype=tf.float32,shape=[None,STATE_NUM])
+        self.input = tf.placeholder(dtype=tf.float32,shape=[None,STATE_NUM],name="input")
 
         #define two network
         with tf.variable_scope("current_brain"):
             hidden1 = tf.layers.dense(self.input,HIDDEN_LAYERE,activation = tf.nn.leaky_relu)
-            hidden1_drop = tf.nn.dropout(hidden1, DROPOUT_RATE)
-            hidden2 = tf.layers.dense(hidden1_drop,LAYERE2,activation = tf.nn.leaky_relu)
-            hidden2_drop = tf.nn.dropout(hidden2, DROPOUT_RATE)
-            hidden3 = tf.layers.dense(hidden2_drop,LAYERE3,activation = tf.nn.leaky_relu)
-            hidden3_drop = tf.nn.dropout(hidden3, DROPOUT_RATE)
-            self.mu = tf.layers.dense(hidden3_drop,ACTION_NUM,activation = tf.nn.tanh)
-            self.sig = tf.layers.dense(hidden3_drop, 1, activation = tf.nn.softplus)
-            self.v=tf.layers.dense(hidden3_drop,1)
+            self.mu = tf.layers.dense(hidden1,ACTION_NUM,activation = tf.nn.tanh)
+            self.sig = tf.layers.dense(hidden1, 1, activation = tf.nn.softplus)
+            self.v=tf.layers.dense(hidden1,1)
         with tf.variable_scope("old_brain"):
             old_hidden1 = tf.layers.dense(self.input,HIDDEN_LAYERE,activation = tf.nn.leaky_relu)
-            old_hidden1_drop = tf.nn.dropout(old_hidden1, DROPOUT_RATE)
-            old_hidden2 = tf.layers.dense(old_hidden1_drop,LAYERE2,activation = tf.nn.leaky_relu)
-            old_hidden2_drop = tf.nn.dropout(old_hidden2, DROPOUT_RATE)
-            old_hidden3 = tf.layers.dense(old_hidden2_drop,LAYERE3,activation = tf.nn.leaky_relu)
-            old_hidden3_drop = tf.nn.dropout(old_hidden3, DROPOUT_RATE)
-            self.old_mu = tf.layers.dense(old_hidden3_drop,ACTION_NUM,activation = tf.nn.tanh)
-            self.old_sig = tf.layers.dense(old_hidden3_drop, 1, activation = tf.nn.softplus)
-            self.old_v=tf.layers.dense(old_hidden3_drop,1)
+            self.old_mu = tf.layers.dense(old_hidden1,ACTION_NUM,activation = tf.nn.tanh)
+            self.old_sig = tf.layers.dense(old_hidden1, 1, activation = tf.nn.softplus)
+            self.old_v=tf.layers.dense(old_hidden1,1)
+        with tf.variable_scope("store_brain"):
+            store_hidden1 = tf.layers.dense(self.input,HIDDEN_LAYERE,activation = tf.nn.leaky_relu)
+            self.store_mu = tf.layers.dense(store_hidden1,ACTION_NUM,activation = tf.nn.tanh)
+            self.store_sig = tf.layers.dense(store_hidden1,1,activation = tf.nn.softplus)
+            self.store_v = tf.layers.dense(store_hidden1,1)
 
-        self.reward=tf.placeholder(dtype=tf.float32,shape=(None,1))
-        self.action=tf.placeholder(dtype=tf.float32,shape=(None,ACTION_NUM))
+        with tf.name_scope("input_train"):
+            self.reward=tf.placeholder(dtype=tf.float32,shape=(None,1))
+            self.action=tf.placeholder(dtype=tf.float32,shape=(None,ACTION_NUM))
 
         #define loss function
-        advantage = self.reward-self.v
+        with tf.name_scope("advantage"):
+            advantage = self.reward-self.v  
 
         #distributions
-        action_dist_new = tf.distributions.Normal(self.mu, self.sig)
-        action_dist_old = tf.distributions.Normal(self.old_mu, self.old_sig)
+        with tf.name_scope("distrobutions"):
+            action_dist_new = tf.distributions.Normal(loc = self.mu, scale = self.sig, allow_nan_stats = False)
+            action_dist_old = tf.distributions.Normal(self.old_mu, self.old_sig, allow_nan_stats = False)
 
         #define policy loss
-        self.prob_new = action_dist_new.prob(self.action) + 1e-10
-        self.prob_old = action_dist_old.prob(self.action) + 1e-10
-        r_theta = tf.div(self.prob_new, tf.stop_gradient(self.prob_old))
-        action_theta = tf.reduce_sum(tf.multiply(r_theta, self.action), axis=1, keepdims=True)
-        r_clip = tf.clip_by_value(action_theta,1-EPSIRON,1+EPSIRON)
-        advantage_cpi = tf.multiply(action_theta , tf.stop_gradient(advantage))
-        advantage_clip = tf.multiply(r_clip , tf.stop_gradient(advantage))
-        self.policy_loss = tf.minimum(advantage_clip , advantage_cpi)
+        with tf.name_scope("probs"):
+            self.prob_new = action_dist_new.prob(self.action) + 1e-10
+            self.prob_old = action_dist_old.prob(self.action) + 1e-10
+        with tf.name_scope("r_theta"):
+            r_theta = tf.div(self.prob_new , tf.stop_gradient(self.prob_old))
+            action_theta = tf.reduce_mean(r_theta, axis=1, keepdims=True)
+        with tf.name_scope("r_clip"):
+            r_clip = tf.clip_by_value(action_theta,1-EPSIRON,1+EPSIRON)
+        with tf.name_scope("CPI"):
+            advantage_cpi = tf.multiply(action_theta , tf.stop_gradient(advantage))
+        with tf.name_scope("CLIP"):
+            advantage_clip = tf.multiply(r_clip , tf.stop_gradient(advantage))
+        with tf.name_scope("policy_loss"):
+            self.policy_loss = tf.minimum(advantage_clip , advantage_cpi)
+            # self.policy_loss = advantage_clip
 
         #define value loss
-        self.value_loss=tf.square(advantage)
+        with tf.name_scope("value_loss"):
+            self.value_loss = tf.square(advantage)
 
         #define entropy
-        self.entropy = action_dist_new.entropy()
+        with tf.name_scope("entropy"):
+            self.entropy = action_dist_new.entropy()
 
         #output loss function
-        self.loss=tf.reduce_mean(-self.policy_loss+LOSS_V*self.value_loss-LOSS_ENTROPY*self.entropy)
+        with tf.name_scope("loss"):
+            self.loss=tf.reduce_mean(-self.policy_loss+LOSS_V*self.value_loss+LOSS_ENTROPY*self.entropy)
 
         #update parameters
-        self.opt = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE)
-        self.minimize = self.opt.minimize(self.loss)
+        with tf.name_scope("optimizer"):
+            self.opt = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE)
+            self.minimize = self.opt.minimize(self.loss)
 
         #get trainable parameters
-        self.weight_param = tf.get_collection(key = tf.GraphKeys.TRAINABLE_VARIABLES, scope="current_brain")
-        self.old_weight_param = tf.get_collection(key = tf.GraphKeys.TRAINABLE_VARIABLES, scope="old_brain")
+        with tf.name_scope("get_prams"):
+            self.weight_param = tf.get_collection(key = tf.GraphKeys.TRAINABLE_VARIABLES, scope="current_brain")
+            self.old_weight_param = tf.get_collection(key = tf.GraphKeys.TRAINABLE_VARIABLES, scope="old_brain")
+            self.store_weight_param = tf.get_collection(key = tf.GraphKeys.TRAINABLE_VARIABLES, scope="store_brain")
 
         #new prarameter assign to old parameter 
-        self.insert = [g_p.assign(l_p) for l_p,g_p in zip(self.weight_param,self.old_weight_param)]
+        with tf.name_scope("insert"):
+            self.store = [g_p.assign(l_p) for l_p,g_p in zip(self.weight_param,self.store_weight_param)]
+            self.insert = [g_p.assign(l_p) for l_p,g_p in zip(self.store_weight_param,self.old_weight_param)]
 
+        with tf.name_scope('summary'):
+            tf.summary.scalar('loss', self.loss)
+            tf.summary.scalar("entropy",tf.reduce_mean(self.entropy))
+            tf.summary.scalar("advantage",tf.reduce_mean(tf.abs(advantage)))
+            tf.summary.scalar("r_theta",tf.reduce_mean(r_theta))
+            tf.summary.scalar("r_clip",tf.reduce_mean(r_clip))
+            tf.summary.scalar("CPI", tf.reduce_mean(advantage_cpi))
+            tf.summary.scalar("CLIP", tf.reduce_mean(tf.abs(advantage_clip)))
+            tf.summary.scalar("policy_loss", tf.reduce_mean(self.policy_loss))
+            tf.summary.scalar("value_loss", tf.reduce_mean(self.value_loss))
+            tf.summary.scalar("sigma", tf.reduce_mean(self.sig))
+            tf.summary.scalar("mu", tf.reduce_mean(self.mu))
+            self.merged = tf.summary.merge_all()
+            self.writer = tf.summary.FileWriter(SUMMARY_PATH, SESS.graph)
 
     def predict(self,state):
-        state=np.array(state).reshape(-1,STATE_NUM)
-        feed_dict={self.input:state}
-        mu,sig,v=SESS.run([self.mu,self.sig,self.v],feed_dict)
+        with tf.name_scope("predict"):
+            state=np.array(state).reshape(-1,STATE_NUM)
+            feed_dict={self.input:state}
+            mu,sig,v=SESS.run([self.mu,self.sig,self.v],feed_dict)
         return mu.reshape(-1,ACTION_NUM),mu.reshape(-1),v.reshape(-1)
 
 
@@ -173,6 +210,7 @@ class ppo_brain:
     #make t 
     def update(self,memory):
         length=len(memory)
+        self.train_num += 1
        
         s_ = np.array([memory[j][0] for j in range(length)]).reshape(-1,STATE_NUM)
         a_ = np.vstack([[memory[j][1] for j in range(length)]])
@@ -183,10 +221,20 @@ class ppo_brain:
         _s = np.array([memory[j][4] for j in range(length)]).reshape(-1,STATE_NUM)
         _, _, v = self.predict(_s)
         R = (np.where(d_,0,1) * v.reshape(-1,1)) * s_mask+R_
+
+
         #update params
         feed_dict={self.input:s_, self.action:a_, self.reward:R}
+        SESS.run(self.store)
+        try:
+            summary, loss, _ = SESS.run([self.merged, self.loss, self.minimize],feed_dict)
+        except:
+            print(self.memory[0],self.memory[1],self.memory[2])
+            assert False
         SESS.run(self.insert)
-        _, loss = SESS.run([self.minimize,self.loss],feed_dict)
+
+        self.writer.add_summary(summary, global_step = self.train_num)
+
         return loss
 
 
@@ -203,7 +251,11 @@ class ppo_agent:
         mu = mu.reshape(-1)
         count=0
         while True:
-            action=np.random.multivariate_normal(mu,sig*np.eye(ACTION_NUM))
+            try:
+                action=np.random.multivariate_normal(mu,sig*np.eye(ACTION_NUM))
+            except:
+                print("error happend")
+                return random(-1,1,size=ACTION_NUM)
             if sum(action>=-1)==ACTION_NUM and sum(action<=1)==ACTION_NUM:
                 return action
             count+=1
@@ -226,6 +278,10 @@ class ppo_agent:
 
     #push observation and action, reward, done, next observation
     def update(self,memory):
+        if len(memory)<ADVANTAGE+1:
+            R = sum([memory[j][2]*(GAMMA**j) for j in range(len(memory))])
+            self.memory.append([memory[0][0],memory[0][1],R,memory[0][3],memory[0][4],GAMMA**ADVANTAGE])
+            return 
         R = sum([memory[j][2]*(GAMMA**j) for j in range(ADVANTAGE+1)])
         self.memory.append([memory[0][0],memory[0][1],R,memory[0][3],memory[0][4],GAMMA**ADVANTAGE])
 
@@ -295,9 +351,7 @@ class Worker:
                 action=self.agent.action(observation)
             
             next_observation,reward,done,_=self.env.step(action)
-            if reward<0:
-                reward /= 3
-            
+
             self.memory.append([observation,action,reward,done,next_observation])
 
             observation=next_observation
@@ -309,14 +363,17 @@ class Worker:
                 loss = self.agent.update(self.memory)
                 self.memory = self.memory[-ADVANTAGE:]
                 trial_rate = step // 100
-                print("Thread:",self.name," Thread_trials:",self.total_trial,"progress",trial_rate,"/10","reward:","{:<010}".format(reward)[:7],"loss:","{:<010}".format(loss)[:7]," total_trial:",total_trial)
+                print("Thread:",self.name," Thread_trials:",self.total_trial,"progress",trial_rate,"/2","reward:","{:.8f}".format(reward)[:7],"loss:","{:.8f}".format(loss)[:7]," total_trial:",total_trial)
             
             if self.thread_type == "test":
                 self.env.render(mode = 0)
 
+            if done:
+                break
+
         self.leaning_memory=np.hstack((self.leaning_memory[1:],step))
 
-        if total_trial%WORKER_NUM==0 and args.save:
+        if total_trial%(WORKER_NUM*5)==0 and args.save:
             saver.save(SESS,MODEL_SAVE_PATH)
             print("saved")
             total_trial+=1
@@ -325,8 +382,12 @@ class Worker:
         else:
             pass
         loss = self.agent.update(self.memory)
+        if loss ==None:
+            loss = 0
         self.memory = []
-        print("Thread:",self.name," Thread_trials:",self.total_trial,"progress","10/10","reward:","{:<010}".format(reward)[:7],"loss:","{:<010}".format(loss)[:7]," total_trial:",total_trial)
+
+        # print("Thread:",self.name," Thread_trials:",self.total_trial,"progress","2 /2","reward:","{:.8f}".format(reward)[:7],"loss:","{:.8f}".format(loss)[:7]," total_trial:",total_trial)
+        print("Thread:",self.name," Thread_trials:","{:8}".format(self.total_trial),"step","{:4}".format(step),"reward:","{:.8f}".format(reward)[:7],"loss:","{:.8f}".format(loss)[:7]," total_trial:",total_trial)
  
 
     
